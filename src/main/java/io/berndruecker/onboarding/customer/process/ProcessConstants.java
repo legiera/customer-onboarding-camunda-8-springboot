0c1979e6459fdd70dc005c53387247aa73c0b982
jadwiga <jadwiga.piechota@onet.pl>
2023-05-23 23:02:13 +0200

    Refactoring the code

--- src/main/java/io/berndruecker/onboarding/customer/process/ProcessConstants.java
+++
@@ -1,9 +0,0 @@
-package io.berndruecker.onboarding.customer.process;
-
-public class ProcessConstants {
-
-  public static final String VAR_SCORING_REQUEST_ID = "scoringRequestId";
-  public static final String VAR_SCORING_RETRY_COUNT = "scoringRetryCount";
-  public static final String VAR_SCORING_RESULT = "scoringResult";
-
-}
